25dd1c4852b618dbc5d47e984314a7e6b1ecc6a2
SebCas07 <tiancas07@gmail.com>
2026-02-06 10:20:33 -0600

    added import request alongside adding the two URL's given

---
+++ weatherTracker.py
@@ -0,0 +1,10 @@
+#imports listed below
+#requirement of text (look up to generate after installing libraries *create it after you finish*)
+import requests
+
+#geocoding endpoint that converts city name to lattitude/longitude 
+URL_One = "https://geocoding-api.open-meteo.com/v1/search" 
+
+#Weather endpoint that returns current temp, wind speed, and timestamp 
+URL_Two = "https://api.open-meteo.com/v1/forecast?current_weather=true"
+
