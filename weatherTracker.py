6c14d31e66ee9bd873ad97610122510cb0135335
SebCas07 <tiancas07@gmail.com>
2026-02-18 08:39:16 -0600

    get cordinates method completed.  *NEEDS FURTHER REVISION*.  Incomplete get_weather function with only paramters.  total test cases: 1

--- weatherTracker.py
+++ weatherTracker.py
@@ -3,8 +3,32 @@
 import requests
 
 #geocoding endpoint that converts city name to lattitude/longitude 
-URL_One = "https://geocoding-api.open-meteo.com/v1/search" 
+def get_coordinates(city, country): 
+    URL_One = "https://geocoding-api.open-meteo.com/v1/search?name={city}&country={country}&count={count}"
 
-#Weather endpoint that returns current temp, wind speed, and timestamp 
+    params = { 
+        "name": city, 
+        "country": country, 
+        "count": 1 
+    }  
+
+    response = requests.get(URL_One, params=params)
+    data = response.json()
+
+    if "results" not in data: 
+        raise Exception("City not found")
+    
+    result = data["results"][0]
+
+    return{ 
+        "city": result["name"], 
+        "country": result["country"], 
+        "latitude": result["latitude"], 
+        "longitude": result["longitude"]
+    }
+
+
 URL_Two = "https://api.open-meteo.com/v1/forecast?current_weather=true"
 
+
+print(get_coordinates("San Diego", "US"))
