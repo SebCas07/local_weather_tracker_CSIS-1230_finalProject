d76adb392350b942e691a7b6a561e92b52a7c8b5
SebCas07 <tiancas07@gmail.com>
2026-03-06 09:37:53 -0600

    Updated file organization by having routes in there own seperate folder

--- routes.py
+++ routes.py
@@ -1,9 +1,11 @@
-
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask (__name__)
 
-#FLASK IMPLEMENTATION (post) 
+@app.route('/')
+def home():
+    return "Welcome to Weather Tracker"
+
 @app.route('/weather', methods = ['POST'])
 def create_weather(): 
     return "Create a new weather report"
@@ -14,12 +16,12 @@ def get_all_weather():
     return "Get all weather reports"
 
 @app.route('/weather/<int:id>', methods = ['GET'])
-def get_weather(id):
+def get_weather_by_id(id):
     return f"Get weather report {id}"
 
 @app.route('/weather/<int:id>', methods = ['PUT'])
 def update_weather(id): 
-    return "Update weather report {id}"
+    return f"Update weather report {id}"
 
 @app.route('/weather/<int:id>', methods = ['DELETE'] )
 def delete_weather(id): 
