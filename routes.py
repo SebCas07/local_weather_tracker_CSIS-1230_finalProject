09a9d034de8ad2325f1b01d9a0ad1c38e0291eb1
SebCas07 <tiancas07@gmail.com>
2026-03-27 01:45:43 -0500

    reworked routes.py to be able to see the values in the SQL table.

--- routes.py
+++ routes.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 #this import is to connect routes to main.py
 from flask import Blueprint
+import psycopg2
 
 app = Flask (__name__)
 
 #variable to import into main.py
 weather_bp = Blueprint('weather', __name__)
+
+def get_db_connection(): 
+    conn = psycopg2.connect(
+        dbname = "weatherTracker_DB_CSIS-1230", 
+        user = "postgres", 
+        password = "Ti@ncas07", 
+        host = "localhost", 
+        port = "5432"
+    )
+    return conn
 
 @weather_bp.route('/')
 def home():
@@ -18,11 +29,52 @@ def create_weather():
 #FLASK IMPLEMENTATION (post) 
 @weather_bp.route('/weather', methods=['GET'])
 def get_all_weather(): 
-    return "Get all weather reports"
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM observations")
+    rows = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    weather_list = []
+    for row in rows: 
+        weather_list.append({
+            "id": row[0], 
+            "city": row[1], 
+            "country": row[2], 
+            "latitude": row[3], 
+            "longitude": row[4], 
+            "temperature": row[5], 
+            "elevation": row[6], 
+            "windspeed": row[7], 
+            "observation_time": row[8]
+        })
+
+    return jsonify(weather_list)
 
 @weather_bp.route('/weather/<int:id>', methods = ['GET'])
 def get_weather_by_id(id):
-    return f"Get weather report {id}"
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM observations WHERE id = %s", (id,))
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    if row is None: 
+        return jsonify({"error": "Record not found"}), 404
+    
+    return jsonify({ 
+        "id": row[0], 
+        "city": row[1], 
+        "country": row[2], 
+        "latitude": row[3], 
+        "longitude": row[4], 
+        "temperature": row[5], 
+        "elevation": row[6], 
+        "windspeed": row[7], 
+        "observation_time": row[8]
+    })
 
 @weather_bp.route('/weather/<int:id>', methods = ['PUT'])
 def update_weather(id): 
